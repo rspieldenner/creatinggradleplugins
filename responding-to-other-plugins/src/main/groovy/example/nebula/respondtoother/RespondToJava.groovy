072973d1b18181f29b3d3d1fcc21c615072ab338
Rob Spieldenner <rob.spieldenner@gmail.com>
2015-06-11 16:39:27 -0700

    Demonstrate plugins assuming other plugins have been applied

--- responding-to-other-plugins/src/main/groovy/example/nebula/respondtoother/RespondToJava.groovy
+++ responding-to-other-plugins/src/main/groovy/example/nebula/respondtoother/RespondToJava.groovy
@@ -2,10 +2,15 @@ package example.nebula.respondtoother
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.tasks.Copy
 
 class RespondToJava implements Plugin<Project> {
-	@Override
-	void apply(Project project) {
-		
-	}
+  @Override
+  void apply(Project project) {
+    // 1. lets try a naive copy just expecting the right things to be in place
+    project.tasks.create('copyCompileLibs', Copy) {
+      from project.configurations.compile
+      into "${project.buildDir}/myCompileClasspath"
+    }
+  }
 }
