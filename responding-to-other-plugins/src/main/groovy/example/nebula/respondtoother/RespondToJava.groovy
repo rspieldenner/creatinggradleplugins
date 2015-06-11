b0a97871a6914e15231a31b021cd5c4ce4899ac9
Rob Spieldenner <rob.spieldenner@gmail.com>
2015-06-11 12:15:39 -0700

    Initial setup for respond to other plugins

---
+++ responding-to-other-plugins/src/main/groovy/example/nebula/respondtoother/RespondToJava.groovy
@@ -0,0 +1,11 @@
+package example.nebula.respondtoother
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class RespondToJava implements Plugin<Project> {
+	@Override
+	void apply(Project project) {
+		
+	}
+}
