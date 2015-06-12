6893ef28cf6e893ccdf35dc468ca92ba459df50a
Rob Spieldenner <rob.spieldenner@gmail.com>
2015-06-12 00:45:46 -0700

    Demonstrate project.plugins.hasPlugin is better but doesn't order

--- responding-to-other-plugins/src/main/groovy/example/nebula/respondtoother/RespondToJava.groovy
+++ responding-to-other-plugins/src/main/groovy/example/nebula/respondtoother/RespondToJava.groovy
@@ -2,15 +2,24 @@ package example.nebula.respondtoother
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.plugins.JavaPlugin
 import org.gradle.api.tasks.Copy
 
 class RespondToJava implements Plugin<Project> {
   @Override
   void apply(Project project) {
     // 1. lets try a naive copy just expecting the right things to be in place
-    project.tasks.create('copyCompileLibs', Copy) {
+    /*project.tasks.create('copyCompileLibs', Copy) {
       from project.configurations.compile
       into "${project.buildDir}/myCompileClasspath"
+    }*/
+
+    // 2. next check if the java plugin has been applied so the compile configuration is in place
+    if (project.plugins.hasPlugin(JavaPlugin)) {
+      project.tasks.create('copyCompileLibs', Copy) {
+        from project.configurations.compile
+        into "${project.buildDir}/myCompileClasspath"
+      }
     }
   }
 }
